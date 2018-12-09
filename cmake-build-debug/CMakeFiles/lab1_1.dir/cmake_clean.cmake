0446b539aa631870be8c4e007f484ff409a513ce
m-michrov <45308469+m-michrov@users.noreply.github.com>
2018-12-09 16:10:23 +0700

    added cmake folder

---
+++ cmake-build-debug/CMakeFiles/lab1_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_1.dir/main.c.obj"
+  "CMakeFiles/lab1_1.dir/finder.c.obj"
+  "lab1_1.pdb"
+  "lab1_1.exe"
+  "lab1_1.exe.manifest"
+  "liblab1_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
